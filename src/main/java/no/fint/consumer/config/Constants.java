9995ea75516b3ba8463af6d015c828ca00e4f5d0
Asgeir Nilsen <asgeir@fintlabs.no>
2020-05-05 07:30:54 +0200

    Version 3.5.0.

--- src/main/java/no/fint/consumer/config/Constants.java
+++ src/main/java/no/fint/consumer/config/Constants.java
@@ -15,10 +15,13 @@ public enum Constants {
     public static final String CACHE_INITIALDELAY_PROGRAMOMRADE = "${fint.consumer.cache.initialDelay.programomrade:960000}";
     public static final String CACHE_FIXEDRATE_PROGRAMOMRADE = "${fint.consumer.cache.fixedRate.programomrade:900000}";
     
-    public static final String CACHE_INITIALDELAY_SKOLE = "${fint.consumer.cache.initialDelay.skole:1020000}";
+    public static final String CACHE_INITIALDELAY_PROGRAMOMRADEMEDLEMSKAP = "${fint.consumer.cache.initialDelay.programomrademedlemskap:1020000}";
+    public static final String CACHE_FIXEDRATE_PROGRAMOMRADEMEDLEMSKAP = "${fint.consumer.cache.fixedRate.programomrademedlemskap:900000}";
+    
+    public static final String CACHE_INITIALDELAY_SKOLE = "${fint.consumer.cache.initialDelay.skole:1080000}";
     public static final String CACHE_FIXEDRATE_SKOLE = "${fint.consumer.cache.fixedRate.skole:900000}";
     
-    public static final String CACHE_INITIALDELAY_UTDANNINGSPROGRAM = "${fint.consumer.cache.initialDelay.utdanningsprogram:1080000}";
+    public static final String CACHE_INITIALDELAY_UTDANNINGSPROGRAM = "${fint.consumer.cache.initialDelay.utdanningsprogram:1140000}";
     public static final String CACHE_FIXEDRATE_UTDANNINGSPROGRAM = "${fint.consumer.cache.fixedRate.utdanningsprogram:900000}";
     
 
