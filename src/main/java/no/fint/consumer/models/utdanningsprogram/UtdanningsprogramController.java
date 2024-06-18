6857aba80b3cdcebf8c6f3afdab1a3a66e1c9b79
Trond Magnus Sevre <76692689+trondsevre@users.noreply.github.com>
2024-06-18 11:27:25 +0200

    Version 3.18.0. (#26)

--- src/main/java/no/fint/consumer/models/utdanningsprogram/UtdanningsprogramController.java
+++ src/main/java/no/fint/consumer/models/utdanningsprogram/UtdanningsprogramController.java
@@ -182,7 +182,7 @@ public class UtdanningsprogramController {
 
             fintAuditService.audit(response, Status.SENT_TO_CLIENT);
 
-            return linker.toResource(utdanningsprogram);
+            return linker.mapAndResetLinks(utdanningsprogram);
         }    
     }
 
